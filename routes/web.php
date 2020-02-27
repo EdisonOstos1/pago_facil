7a837f7060eb7c3b4140ea3dee9cd36449f47848
EdisonOstos1 <ediostos@gmail.com>
2020-02-26 21:34:10 -0500

    Arreglos

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,43 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('arreglosm', function () {
+
+    $paises = [
+        "colombia" => [
+            "capital" => "Bogota",
+            "moneda" => "peso",
+            "poblacion" => 50,
+            "Ciudades Principales" => ["Bogota", "Medellin", "Cali"]
+        ],
+
+        "alemania" => [
+            "capital" => "Berlin",
+            "Moneda" => "Euro",
+            "Poblacion" => "82,79 Millones"
+        ],
+
+
+        "peru" => [
+            "capital" => "Lima",
+            "Moneda" => "Sol",
+            "Polacion" => "32,17 millones"
+        ]
+    ];
+
+    //      echo "<pre>";
+    //    var_dump($paises);
+    //    echo "<pre>";
+
+    // REcorrer Arreglo
+
+    foreach ($paises as $pais => $informacion) {
+
+        echo "<h1> $pais</h1>";
+        foreach ($informacion as $clave => $valor) {
+
+            echo"$clave: $valor";
+        };
+        echo "<hr />";
+    };
+});
